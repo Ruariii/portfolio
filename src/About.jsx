6ef892dde76ba971f26a2ad3105515f601a9b791
Ruari Hodgin <ruarihodgin99@outlook.com>
2024-10-09 22:12:07 +0100

    changed about page

--- src/About.jsx
+++ src/About.jsx
@@ -12,17 +12,17 @@ const About = () => {
       {/* Personal Profile Paragraph */}
       <p className="personal-profile">
       I'm Ruari, a first class aeronautical engineering masters graduate from The University of Leeds. 
-      Currently working in medical device development at Lucid Innovation, a UK SME. 
+      I am currently working as a design engineer in medical and life science industry at Lucid Innovation, a UK SME. 
       I am a versatile engineer with experience in software development, 
       design & manufacture and data analysis applied to fully connected medical devices. 
       I strive to be an integral team member, and have contributed to numerous technical projects in medical engineering; 
-      from the concept stage right through to production. 
+      from concept stage right through to production. 
       I have a strong academic background in mechanical engineering and hands-on experience in design 
       and analysis of aircraft systems. 
       </p>
       <p className="personal-profile">
       This portfolio was created as a personal project using React and Firebase, 
-      to showcase some of the work I have been involved with both at university and since graduating.
+      to showcase some of the work I have been involved with while at university and since graduating.
 
       </p>
     </div>
